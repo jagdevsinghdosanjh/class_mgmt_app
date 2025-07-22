f95f7db10e40d2ed7fc464e1e871a4e4b917fb65
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-07-23 00:40:24 +0530

    Ok

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # MONGO_URI = "mongodb+srv://<username>:<password>@cluster.mongodb.net/class_mgmt"
-#MONGO_URI = "mongodb://localhost:27017/class_mgmt"
+MONGO_URI = "mongodb://localhost:27017/class_mgmt"
 #mongodb+srv://jagdevsinghdosanjh:ndxjuLnqz4oCcg8z@cluster0.3xnlzlw.mongodb.net/
-MONGO_URI = "mongodb+srv://jagdevsinghdosanjh:ndxjuLnqz4oCcg8z@cluster0.3xnlzlw.mongodb.net/class_mgmt?retryWrites=true&w=majority"
+#MONGO_URI = "mongodb+srv://jagdevsinghdosanjh:ndxjuLnqz4oCcg8z@cluster0.3xnlzlw.mongodb.net/class_mgmt?retryWrites=true&w=majority"
 
